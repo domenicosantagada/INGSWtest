fa167c4d1fdbd04f514329202e562854877b0d81
domenicosantagada <domenico.santgada@libero.it>
2022-10-21 22:38:49 +0200

    Update HttpClient.java

--- src/main/java/it/unical/ingsw2021/HttpClient.java
+++ src/main/java/it/unical/ingsw2021/HttpClient.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.MediaType;
-import okhttp3.OkHttpClient;ff
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
